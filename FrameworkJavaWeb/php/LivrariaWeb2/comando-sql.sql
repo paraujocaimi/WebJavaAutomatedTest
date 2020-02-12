5d6fe8b853e5e1211ee3fc52edd380eabd3b1b75
Priscila Caimi <paraujocaimi@hotmail.com>
2020-02-12 16:11:48 -0300

    eliminando pasta php

--- FrameworkJavaWeb/php/LivrariaWeb2/comando-sql.sql
+++
@@ -1,8 +0,0 @@
-create database livraria;
-CREATE TABLE livros( 
-	idlivros int NOT NULL AUTO_INCREMENT, 
-	ISBN varchar(45) NOT NULL, 
-	autor varchar(45) NOT NULL, 
-	titulo varchar(45) NOT NULL, 
-	valor decimal(15,2) NOT NULL, 
-	primary KEY(idlivros) )
